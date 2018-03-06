ae4bd7e2aa4188bad05e7f2bd2a8a8ce05d325ea
Noi OBrien <noitobrien@gmail.com>
2018-03-06 07:40:12 -0600

    initial commit

---
+++ BinarySearchTree/Node.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Node
+    {
+        //member variables
+        public int data;
+        public Node Right;
+        public Node Left;
+        //constructor
+
+        public Node()
+        {
+            Node countNode = new countNode(data);
+        }
+        //member methods
+        public void addData(Node);
+        {
+        if  (node == null)
+            node = nodenewAdd;
+        }
+        else 
+        {
+        }    
+
+
+
+
+    }
+}
